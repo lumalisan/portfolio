e54e4d32509a05515ac1b0d211dc172ed940b183
Lisandro Rocha Tau <lumalisan@gmail.com>
2023-11-29 16:24:34 +0100

    fixed error with &apos;

--- lib/components/About.tsx
+++ lib/components/About.tsx
@@ -48,22 +48,22 @@ const About = () => {
 
       <p
         className="mt-4 text-beige/90 text-xl leading-8">
-        Hello! I'm Lisandro, and my interest in web development traces back to my early days,
-        tinkering with my father's computer. This childhood curiosity ignited a journey that
+        Hello! I&apos;m Lisandro, and my interest in web development traces back to my early days,
+        tinkering with my father&apos;s computer. This childhood curiosity ignited a journey that
         led me to become a computer engineer, graduating from the
         <Link href={'https://www.uib.es/es/'} className='text-blue hover:text-sky-200 transition-colors duration-300'> University of the Balearic Islands (UIB)</Link>.
-        Throughout my professional journey, I've had the privilege of working
-        in diverse companies, where I've not only implemented my skills and knowledge but
+        Throughout my professional journey, I&apos;ve had the privilege of working
+        in diverse companies, where I&apos;ve not only implemented my skills and knowledge but
         also continuously improved them.
         <br/><br/>
-        In addition to my professional experiences, I've embarked on various personal projects,
+        In addition to my professional experiences, I&apos;ve embarked on various personal projects,
         aimingto enhance my abilities and delve into emerging web technologies. This dynamic blend of
         academic background, professional engagement, and personal exploration fuels my passion for
-        creating innovative and impactful web solutions. I'm excited about the ever-evolving landscape
+        creating innovative and impactful web solutions. I&apos;m excited about the ever-evolving landscape
         of web development and committed to building on my experiences to craft accessible and
         cutting-edge digital products.
         <br/><br/>
-        Here are a few technologies I've been working with recently:
+        Here are a few technologies I&apos;ve been working with recently:
       </p>
 
       <div className="mt-6 flex flex-wrap xs:gap-8 gap-2">
