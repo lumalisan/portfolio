e8830e7a12434ed7b814e1b15cac5c5b4c0cbdc6
Lisandro Rocha Tau <lumalisan@gmail.com>
2023-12-02 10:38:01 +0100

    revert test change

--- lib/components/CustomTimelineElement.tsx
+++ lib/components/CustomTimelineElement.tsx
@@ -36,7 +36,6 @@ const CustomTimelineElement = ({
           boxShadow:
             'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
         }}
-        className='test'
         contentArrowStyle={{ borderRight: '7px solid  #363062' }}
         //@ts-ignore
         date={
